00cb4c1d6199ff186ab22abae48b4329c58bc61a
Nurbek Ataboyev <nurbek.ataboyev.prog@gmail.com>
2024-08-24 04:04:29 +0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Nurbek on 24/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
