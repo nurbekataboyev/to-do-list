230ffb038cbb9d455182215958b626bcc2b0a655
Nurbek Ataboyev <nurbek.ataboyev.prog@gmail.com>
2024-08-27 18:28:42 +0500

    fix: dateFormatter to universal

--- ToDoList/Extensions/Date+Ext.swift
+++ ToDoList/Extensions/Date+Ext.swift
@@ -11,10 +11,9 @@ extension Date {
     
     func toCustomFormat() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ru_RU")
         dateFormatter.dateFormat = "LLLL d, HH:mm"
         
-        return dateFormatter.string(from: self).capitalized
+        return dateFormatter.string(from: self)
     }
     
 }
